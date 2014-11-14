b68aa59d612eceff0fb7a073f9990649a6f96200
James Halliday <mail@substack.net>
2014-11-13 16:17:17 -0800

    more retries before removing the old worker url

--- index.js
+++ index.js
@@ -3,48 +3,39 @@ var isarray = require('isarray');
 
 module.exports = function () {
     var prefix = '_pageBus!';
-    var code = new Blob(
-        [ '(' + worksrc + ')()' ],
-        { type: 'text/javascript' }
-    );
-    
-    var ucode = localStorage.getItem(prefix + 'URL');
-    if (!ucode) ucode = URL.createObjectURL(code);
-    
     var times = 0;
-    function createWorker (cb) {
-        try { var worker = new SharedWorker(ucode) }
-        catch (err) {
-            ucode = null;
-            return setTimeout(function () {
-                ucode = getOrLoad();
-                createWorker(cb);
-            }, Math.min(++times * 50, 500));
-        }
-        worker.port.addEventListener('message', onmessage);
+    
+    (function retry () {
+        var ucode = localStorage.getItem(prefix + 'URL');
+        if (!ucode) ucode = createURL();
         
-        var to = setTimeout(function () {
-            // DEAD, try again
-            worker.port.removeEventListener('message', onmessage);
-            ucode = getOrLoad();
-            createWorker(cb);
-        }, Math.min(++times * 50, 500));
+        try { var worker = new SharedWorker(ucode) }
+        catch (err) { setTimeout(retry, Math.min(++times * 100, 500)) }
+        
+        var to = setTimeout(ontimeout, Math.min(++times * 100, 500));
+        worker.port.addEventListener('message', onmessage);
         worker.port.start();
         
         function onmessage (msg) {
             worker.port.removeEventListener('message', onmessage);
             clearTimeout(to);
-            cb(worker);
+            
+            if (localStorage.getItem(prefix + 'URL') !== ucode) {
+                console.log('fuck fuck fuck');
+                retry();
+            }
+            else onworker(worker, ucode);
         }
-    }
-    
-    function getOrLoad () {
-        var ncode = localStorage.getItem(prefix + 'URL');
-        if (ucode !== ncode) {
-            return ncode; // a worker was set up elsewhere
+        
+        function ontimeout () {
+            // DEAD, try again
+            worker.port.removeEventListener('message', onmessage);
+            if (times > 5 && ucode === localStorage.getItem(prefix + 'URL')) {
+                localStorage.removeItem(prefix + 'URL');
+            }
+            retry();
         }
-        return URL.createObjectURL(code);
-    }
+    })();
     
     var emitter = new EventEmitter;
     var queue = [];
@@ -52,10 +43,21 @@ module.exports = function () {
         queue.push(arguments);
         return emitter;
     };
+    return emitter;
     
-    createWorker(function (w) {
-        emitter.emit('_connect', ucode);
+    function createURL () {
+        var code = new Blob(
+            [ '(' + worksrc + ')()' ],
+            { type: 'text/javascript' }
+        );
+        var ucode = URL.createObjectURL(code);
         localStorage.setItem(prefix + 'URL', ucode);
+        return ucode;
+    }
+    
+    function onworker (w, ucode) {
+        emitter.emit('_connect', ucode);
+        
         w.port.addEventListener('message', function (ev) {
             if (isarray(ev.data) && ev.data[0] === prefix) {
                 EventEmitter.prototype.emit.apply(emitter, ev.data.slice(1));
@@ -71,8 +73,7 @@ module.exports = function () {
             emitter.emit.apply(emitter, q);
         });
         queue = null;
-    });
-    return emitter;
+    }
 };
 
 function worksrc () {
